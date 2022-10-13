18ec5067c23db54b52e45f131bd82387ce43b529
neelabh11 <neelabh.pandey11@gmail.com>
2022-10-13 17:45:40 +0530

    Comapny Employee Wages

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            TotalWhd whd = new TotalWhd();
-            whd.CalculateTot();
+            EmpWageBuilerArray empWageBuilerArray = new EmpWageBuilerArray();
+
+            empWageBuilerArray.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilerArray.addCompanyEmpWage("Reilance", 10, 4, 20);
+
+            empWageBuilerArray.computeEmpWage();
         }
     }
 }
