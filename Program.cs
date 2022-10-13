6d2fa374c04e70a4b1e7c7163a58e3d462afd0dd
neelabh11 <neelabh.pandey11@gmail.com>
2022-10-13 13:34:47 +0530

    Checking Employee Attendance

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeCheck emp = new EmployeeCheck();
+            emp.EmployeeAttendance();
+        }
+    }
+}
