24833fdec2ef82b886b60b40a78604330cd03f4b
neelabh11 <neelabh.pandey11@gmail.com>
2022-10-13 13:53:10 +0530

    Calculating Daily Wage

---
+++ EmpDailyWage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmpDailyWage
+    {
+        public void CalculateWage()
+        {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is full time");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee not worked");
+                empHrs = 0;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+
+            Console.WriteLine("EmpWage : " + empWage);
+        }
+    }
+}
